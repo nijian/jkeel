0430260f4935619e1063e361950b3dd1cf09cefd
nijian <org.jkeel@outlook.com>
2019-03-29 16:58:22 +0800

    init

--- jkeel-algorithms/jkeel-algorithms-api/src/main/java/com/github/nijian/jkeel/algorithms/debug/Output.java
+++ jkeel-algorithms/jkeel-algorithms-api/src/main/java/com/github/nijian/jkeel/algorithms/debug/Output.java
@@ -3,12 +3,18 @@ package com.github.nijian.jkeel.algorithms.debug;
 /**
  * Output is for debugging.
  *
- * @param <R> calculation result
- * @param <S> output specification
+ * @param <R> calculation result type
+ * @param <S> output specification type
  * @author nj
  * @since 0.0.1
  */
 public interface Output<R, S> {
 
+    /**
+     * Write calculation result to output with specification
+     *
+     * @param result calculation result
+     * @param spec   output specification
+     */
     void write(R result, S spec);
 }
