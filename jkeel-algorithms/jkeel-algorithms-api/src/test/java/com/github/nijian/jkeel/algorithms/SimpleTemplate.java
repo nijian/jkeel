8859d081f97d2f5a870c8b8281b754f6694a49df
nijian <org.jkeel@outlook.com>
2019-04-22 21:24:35 +0800

    init

--- jkeel-algorithms/jkeel-algorithms-api/src/test/java/com/github/nijian/jkeel/algorithms/SimpleTemplate.java
+++
@@ -1,9 +0,0 @@
-package com.github.nijian.jkeel.algorithms;
-
-public class SimpleTemplate implements  AlgorithmTemplate {
-
-    @Override
-    public String getCid() {
-        return null;
-    }
-}
