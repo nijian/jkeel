ddb38b8ebafa0b5450a14456891618a7dc0e7d23
nijian <org.jkeel@outlook.com>
2019-06-24 17:47:50 +0800

    init

--- jkeel-cache/src/main/java/com/github/nijian/jkeel/cache/CacheService.java
+++ jkeel-cache/src/main/java/com/github/nijian/jkeel/cache/CacheService.java
@@ -1,6 +1,5 @@
 package com.github.nijian.jkeel.cache;
 
-import com.github.nijian.jkeel.commons.Const;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,16 +11,31 @@ import javax.cache.spi.CachingProvider;
 import java.net.URI;
 import java.util.Properties;
 
+/**
+ * Simple wrapper for javax.cache lib. The benefits are:
+ * <ul>
+ * <li>Cache name/id is managed</li>
+ * <li>Cache creation is managed</li>
+ * </ul>
+ *
+ * @author nj
+ * @since 0.0.7
+ */
 public class CacheService {
 
     private static Logger logger = LoggerFactory.getLogger(CacheService.class);
 
     public Cache<String, ?> getCache(final String cacheId, final Properties env) {
-        return getCache(cacheId, null, null, env);
+        return getCache(cacheId, null, (String) null, env);
     }
 
     public Cache<String, ?> getCache(final String cacheId, final URI configURI, final Properties env) {
         return getCache(cacheId, null, configURI, env);
+    }
+
+    public Cache<String, ?> getCache(final String cacheId, final String cachingProviderName, final String configClassPath,
+                                     final Properties env) {
+        return null;
     }
 
     public Cache<String, ?> getCache(final String cacheId, final String cachingProviderName, final URI configURI,
---
+++ jkeel-cache/src/main/java/com/github/nijian/jkeel/cache/Const.java
@@ -0,0 +1,19 @@
+package com.github.nijian.jkeel.cache;
+
+/**
+ * Constants.
+ *
+ * @author nj
+ * @since 0.0.7
+ */
+public interface Const {
+
+    String CACHING_PROVIDER_NAME_KEY = "CachingProvider";
+
+    String CACHING_CONFIG_FILE_NAME_KEY = "CacheConfigFileName";
+
+    String CACHING_PROVIDER_NAME = "org.ehcache.jsr107.EhcacheCachingProvider";
+
+    String CACHING_CONFIG_FILE_NAME = "/ehcache.xml";
+
+}
