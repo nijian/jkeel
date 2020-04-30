70fb97a0e2fe1a4e78df4e1e45ff68ed78887908
nijian <org.jkeel@outlook.com>
2020-04-30 17:34:16 +0800

    init

--- jkeel-commons/src/main/java/com/github/nijian/jkeel/commons/json/service/JsonService.java
+++ jkeel-commons/src/main/java/com/github/nijian/jkeel/commons/json/service/JsonService.java
@@ -2,14 +2,16 @@ package com.github.nijian.jkeel.commons.json.service;
 
 import com.github.nijian.jkeel.commons.JsonString;
 import com.github.nijian.jkeel.commons.ObjectHolder;
+import com.github.nijian.jkeel.concept.ConfigItem;
 import com.github.nijian.jkeel.concept.Service;
+import com.github.nijian.jkeel.concept.ServiceContext;
 
 public final class JsonService extends Service<JsonString> {
 
     @Override
-    protected JsonString handleResult(Object f) {
+    protected JsonString handleResult(ServiceContext<?> ctx, ConfigItem<?> currentBehaviorConfig, Object value) {
         try {
-            return new JsonString(ObjectHolder.objectMapper.writeValueAsString(f));
+            return new JsonString(ObjectHolder.objectMapper.writeValueAsString(value));
         } catch (Exception e) {
             throw new RuntimeException("xxx");
         }
